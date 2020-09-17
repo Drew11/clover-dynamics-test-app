e09ed1081d815458dfaf5450bb5ed5c7ebce2127
Drew11 <drew.naumenko@gmail.com>
2020-09-17 11:56:40 +0300

    Updates

---
+++ precache-manifest.07c89c792db54272f8063a44233ba1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f087f1362bd31d0e54ea635c581b11",
+    "url": "/clover-dynamics-test-app/index.html"
+  },
+  {
+    "revision": "bc69e38bdaa91b015891",
+    "url": "/clover-dynamics-test-app/static/css/main.ba12d39a.chunk.css"
+  },
+  {
+    "revision": "04e4c7f7cbe41dab461a",
+    "url": "/clover-dynamics-test-app/static/js/2.056a7cf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clover-dynamics-test-app/static/js/2.056a7cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc69e38bdaa91b015891",
+    "url": "/clover-dynamics-test-app/static/js/main.e804dd47.chunk.js"
+  },
+  {
+    "revision": "2e549b638ec079122e05",
+    "url": "/clover-dynamics-test-app/static/js/runtime-main.70fefbd6.js"
+  }
+]);
